3b7a01fac91d71f6ba41a4a29ba3be7abec43962
Mattias Karlsson <matkar@wcom.se>
2015-12-11 17:21:51 +0100

    Project created

---
+++ src/Cake.Git/GitAliases.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cake.Core.Annotations;
+
+namespace Cake.Git
+{
+    /// <summary>
+    /// Contains functionality for working with GIT using LibGit2 &amp; LibGit2Sharp
+    /// </summary>
+    [CakeAliasCategory("Git")]
+    // ReSharper disable once UnusedMember.Global
+    public static class GitAliases
+    {
+    }
+}
