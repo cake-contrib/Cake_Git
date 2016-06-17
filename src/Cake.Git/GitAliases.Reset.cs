a39d113c7052ab19ccfdd01296e40010b1741df5
David Brower <david.brower@headquarters.maclean.co.uk>
2016-06-17 10:55:17 +0100

    Added two Reset method overloads. Added Local-Tests task.
    GitReset(ResetMode) which resets the current branch HEAD to the latest
    commit and, optionally, modifies the index and working tree.
    
    GitReset(ResetMode, CommitId) which resets the current branch HEAD to the
    specified commit and, optionally, modifies the index and working tree.
    
    $Target can be passed a value of Local-Tests which runs all tests except
    Git-Clone. The purpose of this is to allow for the situation where
    someone is behind a corporate proxy and the test repository on GitHub
    cannot be cloned (see https://github.com/libgit2/libgit2/issues/2106).

---
+++ src/Cake.Git/GitAliases.Reset.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Cake.Core;
+using Cake.Core.Annotations;
+using Cake.Core.IO;
+using Cake.Git.Extensions;
+using LibGit2Sharp;
+
+namespace Cake.Git
+{
+    public static partial class GitAliases
+    {
+        /// <summary>
+        /// Sets the current branch head (HEAD) to a specified commit, 
+        /// optionally modifying index and working tree to match.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="repositoryDirectoryPath">Path to repository.</param>
+        /// <param name="mode">The effect that the reset action should have on the the index and working tree.</param>
+        /// <param name="commitId">The id of the commit to which the current branch HEAD should be set.</param>
+        [CakeMethodAlias]
+        [CakeAliasCategory("Reset")]
+        public static void GitReset(
+            this ICakeContext context,
+            DirectoryPath repositoryDirectoryPath,
+            GitResetMode mode,
+            string commitId)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (repositoryDirectoryPath == null)
+            {
+                throw new ArgumentNullException(nameof(repositoryDirectoryPath));
+            }
+
+            context.UseRepository(repositoryDirectoryPath,
+                repository => repository.Reset((ResetMode) mode, repository.Lookup<Commit>(commitId)));
+        }
+
+        /// <summary>
+        /// Resets the current branch head (HEAD) optionally modifying index and working tree to match.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="repositoryDirectoryPath">Path to repository.</param>
+        /// <param name="mode">The effect that the reset action should have on the the index and working tree.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        [CakeMethodAlias]
+        [CakeAliasCategory("Reset")]
+        public static void GitReset(
+            this ICakeContext context,
+            DirectoryPath repositoryDirectoryPath,
+            GitResetMode mode)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (repositoryDirectoryPath == null)
+            {
+                throw new ArgumentNullException(nameof(repositoryDirectoryPath));
+            }
+
+            context.UseRepository(repositoryDirectoryPath,
+                repository => repository.Reset((ResetMode)mode));
+        }
+    }
+}
