042d51113a7cb02a4ae48cc8e4caacbc01ed01d5
Pascal Berger <pascal.berger@gmail.com>
2017-02-10 09:57:56 +0100

    Add GitHasUncommitedChanges alias

--- src/Cake.Git/GitAliases.Repository.cs
+++ src/Cake.Git/GitAliases.Repository.cs
@@ -2,13 +2,14 @@
 using Cake.Core;
 using Cake.Core.Annotations;
 using Cake.Core.IO;
+using Cake.Git.Extensions;
 using LibGit2Sharp;
 // ReSharper disable UnusedMember.Global
 
 namespace Cake.Git
 {
     public static partial class GitAliases
-    {
+    { 
         /// <summary>
         /// Checks if a specific directory is a valid Git repository.
         /// </summary>
@@ -42,6 +43,43 @@ namespace Cake.Git
             }
 
             return Repository.IsValid(path.FullPath);
+        }
+
+        /// <summary>
+        /// Checks if a repository contains uncommited changes.
+        /// </summary>
+        /// <example>
+        /// <code>
+        ///     var result = GitHasUncommitedChanges("c:/temp/cake");
+        /// </code>
+        /// </example>
+        /// <param name="context">The context.</param>
+        /// <param name="path">Path to the repository to check.</param>
+        /// <returns>True if the Git repository contains uncommited changes.</returns>
+        /// <exception cref="ArgumentNullException">If any of the parameters are null.</exception>
+        /// <exception cref="RepositoryNotFoundException">If path doesn't exist.</exception>
+        [CakeMethodAlias]
+        [CakeAliasCategory("Repository")]
+        public static bool GitHasUncommitedChanges(this ICakeContext context, DirectoryPath path)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (path == null)
+            {
+                throw new ArgumentNullException(nameof(path));
+            }
+
+            if (!context.FileSystem.Exist(path))
+            {
+                throw new RepositoryNotFoundException($"Path '{path}' doesn't exists.");
+            }
+
+            return context.UseRepository(
+                path,
+                repository => repository.RetrieveStatus().IsDirty);
         }
     }
 }
